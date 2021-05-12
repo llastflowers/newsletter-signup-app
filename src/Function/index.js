ccf71e42e42746a1631cc5aabb639e757ea1910c
Brittany Houtz <brittany@stackery.io>
2021-05-12 14:09:43 -0700

    add qs dependency to package.json

--- src/Function/index.js
+++ src/Function/index.js
@@ -1,23 +1,22 @@
-const qs = require('qs'); // query string interpreter
-const AWS = require('aws-sdk'); // we didn't have to add this like qs this since it's standard on all functions
+const qs = require('qs');
+const AWS = require('aws-sdk');
 exports.handler = async message => {
   console.log(message);
-  const formData = qs.parse(message.body); // the 'message' is everything we got from the api endpoint. QS makes it easy to parse out the form data from the body
-  console.log(formData); // you can delete this in production
+  const formData = qs.parse(message.body);
+  console.log(formData);
 
-  const dynamodb = new AWS.DynamoDB(); // because of our permissions setup, we don't have to specify *which* DB
+  const dynamodb = new AWS.DynamoDB();
   const params = {
     Item: {
       'email': {
-        S: formData.email // S: indicates to DynamoDB this will be a string
+        S: formData.email
       }
     },
     ReturnConsumedCapacity: 'TOTAL',
-    TableName: process.env.TABLE_NAME // uses the env variable that Stackery defined
+    TableName: process.env.TABLE_NAME
   };
-  await dynamodb.putItem(params).promise(); // save to the DB (with a node 8-style await)
+  await dynamodb.putItem(params).promise();
 
-  // we need to return a response or the browser will show an error
   return {
     statusCode: 302,
     headers: {'Location': 'https://stackery.io'}
